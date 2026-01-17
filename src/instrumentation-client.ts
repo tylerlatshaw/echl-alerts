62cb83037bea74b5e6645d233b562e1165283acb
Tyler Latshaw <tylerlatshaw@gmail.com>
2026-01-17 16:07:36 -0500

    Updated sentry release logging

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -5,8 +5,6 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://2045591fc964cca08d7d95604d9b35dd@o4506043986280448.ingest.us.sentry.io/4510727009468416",
-
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
   // Enable logs to be sent to Sentry
@@ -15,6 +13,10 @@ Sentry.init({
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
+
+  release: process.env.NEXT_PUBLIC_SENTRY_RELEASE || "local",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  environment: process.env.NODE_ENV || "local",
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
